855ff4449a3a82249ffaa7e3933d7c81c343bda5
Arshankhanifar <arshankhanifar@gmail.com>
2020-09-14 15:47:55 -0400

    bufferbloat

---
+++ bufferbloat/precache-manifest.69df9226b65572290bdc386447b866c6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ebd5790935e91b553d8724fa6849362",
+    "url": "/index.html"
+  },
+  {
+    "revision": "46e95039f669b058549b",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "e0d45731eed853412abb",
+    "url": "/static/css/main.7d8a2eef.chunk.css"
+  },
+  {
+    "revision": "46e95039f669b058549b",
+    "url": "/static/js/2.41846a83.chunk.js"
+  },
+  {
+    "revision": "8b81bc38395cca664a526c4e304a6603",
+    "url": "/static/js/2.41846a83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0d45731eed853412abb",
+    "url": "/static/js/main.6c526522.chunk.js"
+  },
+  {
+    "revision": "7acb094f2200bd08c32e",
+    "url": "/static/js/runtime-main.a58c2234.js"
+  }
+]);
