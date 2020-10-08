f39a2a4bcc84829e2384ccbe187bba8859f3f268
Arshankhanifar <arshankhanifar@gmail.com>
2020-10-08 15:42:30 -0400

    update

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4123b37c7272dfcbafa768de81a970ab.js"
+  "/precache-manifest.e762261c44ae090106179e440e0619b1.js"
 );
 
 self.addEventListener('message', (event) => {
