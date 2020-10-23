872f44a7019d7c82641665c84c722d05664fe83e
Arshankhanifar <arshankhanifar@gmail.com>
2020-10-23 11:35:26 -0400

    update

---
+++ precache-manifest.e1d0812400c5b39abb1ecf5fbbe86d59.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d16469455a675e72d6dbdf0d81914b71",
+    "url": "https://arshankhanifar.github.io/index.html"
+  },
+  {
+    "revision": "aa624e7a329e1a7fe0b7",
+    "url": "https://arshankhanifar.github.io/static/css/2.3af5a994.chunk.css"
+  },
+  {
+    "revision": "83ec401afe89ea0444c9",
+    "url": "https://arshankhanifar.github.io/static/css/main.2397dbb8.chunk.css"
+  },
+  {
+    "revision": "aa624e7a329e1a7fe0b7",
+    "url": "https://arshankhanifar.github.io/static/js/2.b3473a97.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "https://arshankhanifar.github.io/static/js/2.b3473a97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83ec401afe89ea0444c9",
+    "url": "https://arshankhanifar.github.io/static/js/main.22ab9160.chunk.js"
+  },
+  {
+    "revision": "101d8bad8278654bc861",
+    "url": "https://arshankhanifar.github.io/static/js/runtime-main.497775f4.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "https://arshankhanifar.github.io/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "https://arshankhanifar.github.io/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "https://arshankhanifar.github.io/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "https://arshankhanifar.github.io/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "https://arshankhanifar.github.io/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4572187efc44722e60072fccdac8b29f.js"
+  "https://arshankhanifar.github.io/precache-manifest.e1d0812400c5b39abb1ecf5fbbe86d59.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://arshankhanifar.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
